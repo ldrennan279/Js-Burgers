2190e4e2cac7637b615605669cb8f5b4737b0f3e
ldrennan279 <leedrennan279@gmail.com>
2023-07-05 09:33:58 +0100

    update 050723

---
+++ main.js
@@ -0,0 +1,16 @@
+const htmlRender = document.getElementById('htmlRender')
+
+htmlRender.innerHTML = `
+        <header>
+            <h1>J's Burgers</h1>
+        </header>
+        <main>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+        </main>
+
+`
