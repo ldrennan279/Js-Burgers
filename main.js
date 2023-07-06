c5c3e788cd07d5cf2e6eb27547960166f12bbe97
ldrennan279 <leedrennan279@gmail.com>
2023-07-06 10:53:14 +0100

    update 060723

--- main.js
+++ main.js
@@ -9,7 +9,17 @@ htmlRender.innerHTML = `
         </header>
         <main>
             <section>
-                
+                <div class="item" id="item">
+                    <img src="images/burger.jpg" alt="picture of burger" class="food">
+                        <div class="description-price">
+                            <div class="food-calorie">
+                                <h3>Beef Burger</h3>
+                                <h5>calories: 240</h5>
+                            </div>
+                            <h3>£4</h3>
+                        </div>
+                    <img src="images/add.png" alt="add_symbol" class="add_symbol">
+                </div>
             </section>
             <section>
 
